f4560a844924aa453a8bfa3380d8d10f3dbe42a1
James Brand <jamesbrandecon@gmail.com>
2025-05-01 19:50:13 -0700

    adding api.md

---
+++ docs/src/api.md
@@ -0,0 +1,25 @@
+# API Documentation
+
+```@docs
+define_problem
+```
+
+```@docs
+make_grid_points
+```
+
+```@docs
+estimate!
+```
+
+```@docs
+subsample!
+```
+
+```@docs
+bootstrap!
+```
+
+```@docs
+plot_coefficients
+```
--- src/grids.jl
+++ src/grids.jl
@@ -1,7 +1,16 @@
 """ 
     make_grid_points(data, linear, nonlinear)
+
 Generates a grid of points for evaluating the FKRB model. Currently a very simple implementation, though easy to 
 extend. 
+
+# # Arguments
+- `data::DataFrame`
+- `linear::Vector{String}`: A vector of variable names from `data` indicating the terms in the utility function.
+- `nonlinear::Vector{String}`: A vector of variable names from `data`. Should match `linear`-- will eventually collapse these into one input.
+
+# Output: 
+- `grid_points::Matrix{Float64}`: A matrix of grid points for the linear/nonlinear variables. Each row corresponds to a grid point, and each column corresponds to a variable.
 """
 function make_grid_points(data, linear, nonlinear; gridspec::FKRBGridDetails)
     all_vars = union(linear, nonlinear);
